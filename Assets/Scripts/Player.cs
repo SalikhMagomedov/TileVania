6577573e978cd52d0be342b7332d7fa2d9ebdf9a
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-02 22:26:28 +0300

    Move Player Horizontally

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float runSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        var controlThrow = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(controlThrow * runSpeed, rb.velocity.y);
+    }
+}
