e56614e440802b0df337e8397974370c8cb730fc
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-03 17:45:27 +0300

    Jumpy Jumpy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float runSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 5f;
 
-    private Rigidbody2D rb;
-    private Animator animator;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    
+    private static readonly int IsRunning = Animator.StringToHash("IsRunning");
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
 
@@ -25,17 +27,27 @@ public class Player : MonoBehaviour
     {
         var controlThrow = Input.GetAxis("Horizontal");
 
-        rb.velocity = new Vector2(controlThrow * runSpeed, rb.velocity.y);
-        animator.SetBool("IsRunning", Mathf.Abs(controlThrow) > Mathf.Epsilon);
+        _rb.velocity = new Vector2(controlThrow * runSpeed, _rb.velocity.y);
+        _animator.SetBool(IsRunning, Mathf.Abs(controlThrow) > Mathf.Epsilon);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            var jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            _rb.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
     {
-        if (Mathf.Abs(rb.velocity.x) > Mathf.Epsilon)
-        {
-            transform.localScale = new Vector3(Mathf.Sign(rb.velocity.x),
-                                               transform.localScale.y,
-                                               transform.localScale.z);
-        }
+        if (!(Mathf.Abs(_rb.velocity.x) > Mathf.Epsilon)) return;
+        
+        var localScale = transform.localScale;
+        localScale = new Vector3(Mathf.Sign(_rb.velocity.x),
+            localScale.y,
+            localScale.z);
+        transform.localScale = localScale;
     }
 }
