26d42efe16005df6ed79dd739bb6672573277e57
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-04 17:17:07 +0300

    Making Enemies

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    
+    private Rigidbody2D _rb;
+
+    private void Start() => _rb = GetComponent<Rigidbody2D>();
+
+    private void Update() => _rb.velocity = new Vector2(IsFacingRight() ? moveSpeed : -moveSpeed, 0f);
+
+    private bool IsFacingRight() => transform.localScale.x > 0;
+
+    private void OnTriggerExit2D(Collider2D other) =>
+        transform.localScale = new Vector2(-(Mathf.Sign(_rb.velocity.x)), 1f);
+}
