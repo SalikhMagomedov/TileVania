3fca2eb63a342184cb17382ab4cee34e468ab797
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-06 15:54:49 +0300

    Start & Success Screens

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartFirstLevel() => SceneManager.LoadSceneAsync(1);
+
+    public void LoadMainMenu() => SceneManager.LoadSceneAsync(0);
+}
