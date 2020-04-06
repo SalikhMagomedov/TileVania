752fcf2084adc20dc7e3dcf64e1ff91cd7b0f98f
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-06 17:07:16 +0300

    Adding Rising Water

---
+++ Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    [Tooltip("Game units per seconds")]
+    [SerializeField] private float scrollRate = .2f;
+
+    private void Update()
+    {
+        transform.Translate(new Vector2(0f, scrollRate * Time.deltaTime));
+    }
+}
