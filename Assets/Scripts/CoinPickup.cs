534e89052485aee0db8b4be9d26279ec3614c398
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-06 16:30:33 +0300

    Persistent SFX

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,8 +2,11 @@
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] private AudioClip coinPickUpSfx;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (Camera.main != null) AudioSource.PlayClipAtPoint(coinPickUpSfx, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
