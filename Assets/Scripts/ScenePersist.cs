c23112e1cb0868f780e95f64d5be27888ea6c51f
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-06 16:56:02 +0300

    Remembering Pickups

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+    private int _startingSceneIndex;
+
+    private void Awake()
+    {
+        var numScenePersist = FindObjectsOfType<ScenePersist>().Length;
+        
+        if (numScenePersist > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        _startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Update()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != _startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
